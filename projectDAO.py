5909810990acfc45de45f5cd5281373938a24a7f
Przemyslaw Bil <g00398317@gmit.ie>
2022-12-21 14:23:02 +0000

    favoritesDAO class created, DAOs tested

--- projectDAO.py
+++ projectDAO.py
@@ -70,7 +70,7 @@ class UserDAO:
 
     def update(self, newName, id):
         cursor = self.getCursor()        
-        sql = "UPDATE user SET name = %s, WHERE user_id=%s"
+        sql = "UPDATE user SET name = %s WHERE user_id=%s"
         values = (newName, id)
         cursor.execute(sql, values)
         self.connection.commit()
@@ -85,4 +85,71 @@ class UserDAO:
 
         self.closeAll()
 
+class FavoritesDAO:
+    host = ""
+    user= ""
+    password = ""
+    database = ""
+
+    connection =""
+    cursor =""
+
+    def __init__(self):
+        self.host = mysqlConfig["host"]
+        self.user = mysqlConfig["user"]
+        self.password = mysqlConfig["password"]
+        self.database = mysqlConfig["database"]
+
+    def getCursor(self):
+        self.connection = mysql.connector.connect(
+            host = self.host,
+            user = self.user,
+            password = self.password,
+            database = self.database,
+            auth_plugin='mysql_native_password'
+        )
+        self.cursor = self.connection.cursor()
+
+        return self.cursor        
+
+    def closeAll(self):
+        self.connection.close()
+        self.cursor.close()       
+
+    # Add new user
+    def create(self, user_id, album_id):
+        cursor = self.getCursor()
+        sql = "INSERT INTO favorites (user_id, album_id) VALUES (%s, %s)"
+        values = (user_id, album_id)
+        cursor.execute(sql, values)
+
+        self.connection.commit()
+        newid = cursor.lastrowid
+        self.closeAll()
+        return newid               
+
+    # Get user favorites by user_id
+    def getFavoritesByUserID(self, user_id):
+        cursor = self.getCursor()
+        sql = "SELECT * FROM favorites WHERE user_id= %s"
+        values = (user_id,)
+
+        cursor.execute(sql, values)
+
+        result = cursor.fetchall()
+        self.closeAll()
+        return result     
+        
+
+    def delete(self, id):
+        cursor = self.getCursor()  
+        sql = "DELETE FROM favorites WHERE id=%s"
+        values = (id, )
+        cursor.execute(sql, values)
+        self.connection.commit()
+
+        self.closeAll()
+
+
 userDAO = UserDAO()
+favoritesDAO = FavoritesDAO()
